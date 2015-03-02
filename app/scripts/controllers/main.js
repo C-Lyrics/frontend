4578ef9087207a69bfd75bc459075656733e1e62
seba-1511 <seba-1511@hotmail.com>
2015-03-02 04:44:42 -0800

    Basic autocomplete is working !

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -45,22 +45,25 @@ angular.module('frontendApp')
             }
             Autocomplete.getArtists(newVal, function(res) {
                 $scope.suggestions = res;
+                jQuery('#autocomplete')
+                    .autocomplete({
+                        source: $scope.suggestions,
+                        select: function(event, ui) {
+                            $scope.currentSearch = ui.item.value;
+                        }
+                    });
             });
         });
-
-
-        $scope.states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas',
-            'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida',
-            'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa',
-            'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland',
-            'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi',
-            'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-            'New Jersey', 'New Mexico', 'New York', 'North Dakota',
-            'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania',
-            'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee',
-            'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington',
-            'West Virginia', 'Wisconsin', 'Wyoming'
-        ];
+        Autocomplete.getArtists('', function(res) {
+            $scope.suggestions = res;
+            jQuery('#autocomplete')
+                .autocomplete({
+                    source: $scope.suggestions,
+                    select: function(event, ui) {
+                        $scope.currentSearch = ui.item.value;
+                    }
+                });
+        });
 
         $scope.generateWC = function() {
             var artist = $scope.currentSearch;
@@ -77,7 +80,8 @@ angular.module('frontendApp')
         };
 
         $scope.addToCloud = function() {
-            var artist = $scope.currentSearch;
+            var artist = jQuery('#autocomplete')
+                .val();
             if (!artist) {
                 alert('Please enter an artist\'s name.');
                 return;
