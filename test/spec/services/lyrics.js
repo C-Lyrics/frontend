ea7ffc0fd48b81c8fdd80fcae0efd907a98cfdc1
Kelsey Fargas <kelsey.fargas@gmail.com>
2015-03-10 22:20:42 -0700

    count number test is done'

--- test/spec/services/lyrics.js
+++ test/spec/services/lyrics.js
@@ -14,12 +14,15 @@ describe('Service: Lyrics', function () {
 
 
   it('should return count number', function () {
-      // var word = 'Kelsey';
-      // var lyrics = 'A Kelsey is the best person ever';
-      var word = 'JayZ';
-      var lyrics = 'I hopped off the plane at L.A.X. With a dream and my cardigan Welcome to the land of fame excess (whoa), Am I gonna fit in? Jumped in the cab, Here I am for the first time Look to my right and I see the Hollywood sign This is all so crazy Everybody seems so famous My tummys turnin and Im feelin kinda home sick Too much pressure and Im nervous, Thats when the taxi man turned on the radio And a JayZ song was on And a JayZ song was on And a JayZ song was on';
-      var wordcount = Lyrics.countFrequency(word, lyrics);
+      var word1 = 'jaYz';
+      var lyrics1 = 'I hopped off the plane at L.A.X. With a dream and my cardigan Welcome to the land of fame excess (whoa), Am I gonna fit in? Jumped in the cab, Here I am for the first time Look to my right and I see the Hollywood sign This is all so crazy Everybody seems so famous My tummys turnin and Im feelin kinda home sick Too much pressure and Im nervous, Thats when the taxi man turned on the radio And a JayZ song was on And a JayZ song was on And a JayZ song was on';
+      var wordcount = Lyrics.countFrequency(word1, lyrics1);
       expect(wordcount).toEqual(3);
+      
+      var word2 = 'ALl';
+      var lyrics2 = 'ALL I WANT IS YOU ALL I WANT IS YOU ALL I WANT IS YOU ALL I WANT IS YOU ALL I WANT IS YOU ALL I WANT IS YOU ALL I WANT IS YOU ALL I WANT IS YOU ALL I WANT IS YOU ALL I WANT IS YOU ALL I WANT IS YOU';
+      var wordcount2 = Lyrics.countFrequency(word2, lyrics2);
+      expect(wordcount2).toEqual(11);
   });
   
   
